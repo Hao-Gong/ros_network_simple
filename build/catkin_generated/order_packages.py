9a9ec50822a664fa199508e5998453224255397d
gong <diamous@163.com>
2017-09-30 21:02:39 +0800

    gong v3

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/gong/桌面/myrobot/myrobot/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/gong/桌面/myrobot/devel;/opt/ros/kinetic".split(';') if "/home/gong/桌面/myrobot/devel;/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/home/gong/桌面/myrobot/myrobot/devel;/opt/ros/kinetic".split(';') if "/home/gong/桌面/myrobot/myrobot/devel;/opt/ros/kinetic" != "" else []
